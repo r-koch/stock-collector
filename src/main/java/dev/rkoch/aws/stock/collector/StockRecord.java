b0c9f53affb4b07e26eb144b6eb88015d6f29f6a
r-koch <me@rkoch.dev>
2025-09-27 14:33:06 +0200

    refactoring and simplification

--- src/main/java/dev/rkoch/aws/stock/collector/StockRecord.java
+++ src/main/java/dev/rkoch/aws/stock/collector/StockRecord.java
@@ -1,34 +1,19 @@
 package dev.rkoch.aws.stock.collector;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import org.apache.parquet.schema.LogicalTypeAnnotation;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName;
 import org.apache.parquet.schema.Types;
+import com.crazzyghost.alphavantage.timeseries.response.StockUnit;
 import blue.strategic.parquet.Dehydrator;
 import dev.rkoch.aws.s3.parquet.ParquetRecord;
 
 public class StockRecord implements ParquetRecord {
 
-  public double getClose() {
-    return close;
-  }
-
-  public double getHigh() {
-    return high;
-  }
-
-  public double getLow() {
-    return low;
-  }
-
-  public double getOpen() {
-    return open;
-  }
-
-  public long getVolume() {
-    return volume;
+  public static StockRecord of(final LocalDate localDate, final String id, final double close, final double high, final double low, final double open,
+      final long volume) {
+    return new StockRecord(localDate, id, close, high, low, open, volume);
   }
 
   public static StockRecord of(final LocalDate localDate, final String id, final String close, final String high, final String low, final String open,
@@ -39,6 +24,11 @@ public class StockRecord implements ParquetRecord {
     double o = Double.parseDouble(open);
     long v = parseLong(volume);
     return new StockRecord(localDate, id, c, h, l, o, v);
+  }
+
+  public static StockRecord of(final String id, final StockUnit stockUnit) {
+    return new StockRecord(LocalDate.parse(stockUnit.getDate()), id, stockUnit.getClose(), stockUnit.getHigh(), stockUnit.getLow(), stockUnit.getOpen(),
+        stockUnit.getVolume());
   }
 
   private static long parseLong(final String value) {
@@ -68,12 +58,41 @@ public class StockRecord implements ParquetRecord {
     this.volume = volume;
   }
 
+  public double getClose() {
+    return close;
+  }
+
+  @Override
+  public Dehydrator<ParquetRecord> getDehydrator() {
+    return (record, valueWriter) -> {
+      valueWriter.write("localDate", localDate.toEpochDay());
+      valueWriter.write("id", id);
+      valueWriter.write("close", close);
+      valueWriter.write("high", high);
+      valueWriter.write("low", low);
+      valueWriter.write("open", open);
+      valueWriter.write("volume", volume);
+    };
+  }
+
+  public double getHigh() {
+    return high;
+  }
+
   public String getId() {
     return id;
   }
 
   public LocalDate getLocalDate() {
     return localDate;
+  }
+
+  public double getLow() {
+    return low;
+  }
+
+  public double getOpen() {
+    return open;
   }
 
   @Override
@@ -89,17 +108,8 @@ public class StockRecord implements ParquetRecord {
     );
   }
 
-  @Override
-  public Dehydrator<ParquetRecord> getDehydrator() {
-    return (record, valueWriter) -> {
-      valueWriter.write("localDate", Long.valueOf(ChronoUnit.DAYS.between(LocalDate.ofEpochDay(0), localDate)).intValue());
-      valueWriter.write("id", id);
-      valueWriter.write("close", close);
-      valueWriter.write("high", high);
-      valueWriter.write("low", low);
-      valueWriter.write("open", open);
-      valueWriter.write("volume", volume);
-    };
+  public long getVolume() {
+    return volume;
   }
 
 }
--- src/main/java/dev/rkoch/aws/stock/collector/api/AlphaVantageApi.java
+++ src/main/java/dev/rkoch/aws/stock/collector/api/AlphaVantageApi.java
@@ -3,7 +3,6 @@ package dev.rkoch.aws.stock.collector.api;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import javax.naming.LimitExceededException;
 import com.crazzyghost.alphavantage.AlphaVantage;
@@ -12,6 +11,7 @@ import com.crazzyghost.alphavantage.parameters.OutputSize;
 import com.crazzyghost.alphavantage.timeseries.response.StockUnit;
 import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse;
 import dev.rkoch.aws.stock.collector.StockRecord;
+import dev.rkoch.aws.stock.collector.exception.NoDataForDateException;
 
 public class AlphaVantageApi {
 
@@ -26,36 +26,32 @@ public class AlphaVantageApi {
     alphaVantage.init(Config.builder().key(System.getenv(ALPHAVANTAGE_API_KEY)).build());
   }
 
-  public StockRecord getData(final LocalDate date, final String symbol) throws LimitExceededException {
+  public StockRecord getData(final LocalDate date, final String symbol) throws LimitExceededException, NoDataForDateException {
+    for (StockUnit stockUnit : getStockUnits(symbol)) {
+      if (LocalDate.parse(stockUnit.getDate()).isEqual(date)) {
+        return StockRecord.of(symbol, stockUnit);
+      }
+    }
+    throw new NoDataForDateException("no data found for %s on date %s".formatted(symbol, date));
+  }
+
+  public List<StockRecord> getData(final String symbol) throws LimitExceededException {
+    return getStockUnits(symbol).stream().map((stockUnit) -> StockRecord.of(symbol, stockUnit)).toList();
+  }
+
+  private List<StockUnit> getStockUnits(final String symbol) throws LimitExceededException {
     List<StockUnit> stockUnits = cache.get(symbol);
     if (stockUnits == null) {
       String apiSymbol = symbol.replace(".", "-");
       TimeSeriesResponse response = alphaVantage.timeSeries().daily().forSymbol(apiSymbol).outputSize(OutputSize.FULL).fetchSync();
       String errorMessage = response.getErrorMessage();
       if (errorMessage != null && !errorMessage.isBlank()) {
-        throw new LimitExceededException("alphavantage limit exceeded for %s".formatted(date));
+        throw new LimitExceededException("alphavantage limit exceeded for %s".formatted(LocalDate.now()));
       }
       stockUnits = response.getStockUnits();
       cache.put(symbol, stockUnits);
     }
-    for (StockUnit stockUnit : stockUnits) {
-      if (LocalDate.parse(stockUnit.getDate()).isEqual(date)) {
-        String close = cleanNumber(stockUnit.getClose());
-        String high = cleanNumber(stockUnit.getHigh());
-        String low = cleanNumber(stockUnit.getLow());
-        String open = cleanNumber(stockUnit.getOpen());
-        String volume = cleanNumber(stockUnit.getVolume());
-        return StockRecord.of(date, symbol, close, high, low, open, volume);
-      }
-    }
-    throw new RuntimeException("no data found for %s on date %s".formatted(symbol, date.toString()));
-  }
-
-  private String cleanNumber(final double value) {
-    String v = String.format(Locale.US, "%.4f", value);
-    v = v.replaceAll("0*$", "");
-    v = v.replaceAll("\\.$", "");
-    return v;
+    return stockUnits;
   }
 
 }
